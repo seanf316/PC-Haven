da4ce3530a3a2a6e3b26cc5f1c70067875bea74e
seanf316 <seanfinn316@gmail.com>
2023-05-11 21:48:29 +0000

    Added missing fields to Product model

--- products/models.py
+++ products/models.py
@@ -50,6 +50,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    stock_level = models.IntegerField(default=0)
+    in_stock = models.BooleanField(default=True)
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.name
