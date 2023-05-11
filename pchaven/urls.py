ee1fdf199a376d861f7900719d479657d1bae70d
seanf316 <seanfinn316@gmail.com>
2023-05-11 20:33:43 +0000

    Testing site security

--- pchaven/urls.py
+++ pchaven/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
     path('accounts/', include("allauth.urls")),
-    path('products/', include("products.urls")),
+    # path('products/', include("products.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
