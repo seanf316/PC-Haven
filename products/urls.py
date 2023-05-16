4a3afde7154eb5dda9125e12aa6e97d66705ed20
seanf316 <seanfinn316@gmail.com>
2023-05-16 00:03:31 +0000

    Products url updated with product detail view

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.allproducts, name='products'),
+    path('products/<int:product_id>', views.product_detail, name='product_detail'),
 ]
