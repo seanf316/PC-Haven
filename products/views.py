1465995064113c08f115bc5d7af4b80f1f46bee1
seanf316 <seanfinn316@gmail.com>
2023-05-16 00:03:05 +0000

    Product Detail view created

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product, CategoryGroup, SubCategory
 
 
 def allproducts(request):
-    """A view to show all prdoucts, including sorting and search queries"""
+    """A view to show all products, including sorting and search queries"""
 
     products = Product.objects.all()
 
@@ -12,3 +12,14 @@ def allproducts(request):
     }
 
     return render(request, "products/products.html", context)
+
+def product_detail(request, product_id):
+    """A view to show product details of a selected product"""
+
+    product = get_object_or_404(Product, pk=product_id)
+   
+    context = {
+        "product": product,
+    }
+
+    return render(request, "products/product_detail.html", context)
