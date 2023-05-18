71e5c10f8ac71a4d8e2c57f3def755556c2c0cb2
seanf316 <seanfinn316@gmail.com>
2023-05-18 11:29:07 +0000

    Added sorting to the allproducts view

--- products/views.py
+++ products/views.py
@@ -12,8 +12,24 @@ def allproducts(request):
     query = None
     categories = None
     subcategories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+        
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -36,12 +52,14 @@ def allproducts(request):
     paginator = Paginator(products, 12)
     page = request.GET.get('page')
     paginated_products = paginator.get_page(page)
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         "products": paginated_products,
         "search_query": query,
         'categories': categories,
         'subcategories': subcategories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, "products/products.html", context)
