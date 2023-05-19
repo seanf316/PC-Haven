73dc88ee2cd035e6614377ba15600d459a036314
seanf316 <seanfinn316@gmail.com>
2023-05-19 18:54:05 +0000

    files formatted with Black

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allproducts, name='products'),
-    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path("", views.allproducts, name="products"),
+    path("<int:product_id>/", views.product_detail, name="product_detail"),
 ]
