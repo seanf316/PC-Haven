f1071fb0c22e945f567d60d6951174a118b652c2
seanf316 <seanfinn316@gmail.com>
2023-05-21 19:59:40 +0000

    product urls updated with edit product view

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.allproducts, name="products"),
     path("<int:product_id>/", views.product_detail, name="product_detail"),
     path("add/", views.add_product, name="add_product"),
+    path("edit/<int:product_id>/", views.edit_product, name="edit_product"),
 ]
