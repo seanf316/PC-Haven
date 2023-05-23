8f083c687910ebb63dd2a9354ef3518f130716fe
seanf316 <seanfinn316@gmail.com>
2023-05-23 20:56:30 +0000

    Add_to_cart view created

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    HttpResponse,
+    get_object_or_404,
+)
+from django.contrib import messages
+from products.models import Product
 
 
 def view_cart(request):
     """A view that renders the cart content page"""
+
+    return render(request, "cart/cart.html")
+
+
+def add_to_cart(request, product_id):
+    """Add a product and its quantity to the cart"""
+
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
+    cart = request.session.get("cart", {})
+
+    if product_id in list(cart.keys()):
+        cart[product_id] += quantity
+    else:
+        cart[product_id] = quantity
+
+    request.session["cart"] = cart
 
     return render(request, "cart/cart.html")
