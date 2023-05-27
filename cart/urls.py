edab293815ed00fa279d69a666580d37ecf06c82
seanf316 <seanfinn316@gmail.com>
2023-05-27 00:46:55 +0000

    Updated urls to remove "int:" - not required

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.view_cart, name="view_cart"),
-    path("add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("add/<product_id>/", views.add_to_cart, name="add_to_cart"),
 ]
