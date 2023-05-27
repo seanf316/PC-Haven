d6b5aa5706372e2a98f60d6e0640e2c13e5737f7
seanf316 <seanfinn316@gmail.com>
2023-05-27 00:47:40 +0000

    Created edit cart view

--- cart/views.py
+++ cart/views.py
@@ -33,3 +33,27 @@ def add_to_cart(request, product_id):
 
     else:
         return HttpResponse("Invalid request")
+
+
+def edit_cart(request, product_id):
+    """Edit quantity of product in the cart"""
+
+    product = get_object_or_404(Product, id=product_id)
+    quantity = int(request.POST.get("quantity"))
+    cart = request.session.get("cart", {})
+
+    if quantity > 0:
+        cart[product_id] = quantity
+        messages.success(
+            request, f"Updated {product.name} quantity to {cart[product_id]}"
+        )
+
+    else:
+        cart.pop(product_id)
+        messages.success(
+            request, f"Removed {quantity} {product.name} from your cart"
+        )
+
+    request.session["cart"] = cart
+
+    return redirect(reverse("view_cart"))
