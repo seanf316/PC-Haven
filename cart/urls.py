72a3f98641d1096595a9f52ec028e31ddb1a587a
seanf316 <seanfinn316@gmail.com>
2023-05-27 00:47:58 +0000

    Updated urls with edit cart view path

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.view_cart, name="view_cart"),
     path("add/<product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("edit/<product_id>/", views.edit_cart, name="edit_cart"),
 ]
