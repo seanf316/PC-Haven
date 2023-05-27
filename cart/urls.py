12fbbe91a6ef4766a8414a2e78796b8330e4c627
seanf316 <seanfinn316@gmail.com>
2023-05-27 00:49:02 +0000

    Urls updated with newly created delete views

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,14 @@ urlpatterns = [
     path("", views.view_cart, name="view_cart"),
     path("add/<product_id>/", views.add_to_cart, name="add_to_cart"),
     path("edit/<product_id>/", views.edit_cart, name="edit_cart"),
+    path(
+        "delete/<product_id>/",
+        views.delete_from_cart,
+        name="delete_from_cart",
+    ),
+    path(
+        "delete-all/",
+        views.delete_all_cart,
+        name="delete_all_cart",
+    ),
 ]
