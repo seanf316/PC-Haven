f71272699ec09033ebde79751dcd83274aad69fe
seanf316 <seanfinn316@gmail.com>
2023-06-04 21:11:21 +0000

    Order model updated with profile reference

--- checkout/models.py
+++ checkout/models.py
@@ -3,11 +3,19 @@ from django.db.models import Sum
 from django.conf import settings
 from django.db import models
 from products.models import Product
+from profiles.models import UserProfile
 from django_countries.fields import CountryField
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders",
+    )
     order_date = models.DateTimeField(auto_now_add=True)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
