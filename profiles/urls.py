2cd424692960ee2ec241583cbd081f37a5503774
seanf316 <seanfinn316@gmail.com>
2023-06-05 22:43:14 +0000

    Profile urls updated with order history path

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.profile, name="profile")]
+urlpatterns = [
+    path("", views.profile, name="profile"),
+    path(
+        "order_history/<order_number>",
+        views.order_history,
+        name="order_history",
+    ),
+]
