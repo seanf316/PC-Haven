3d5b4a52c1fa1c39b063a688cea76ceb912850ee
seanf316 <seanfinn316@gmail.com>
2023-06-07 02:38:38 +0000

    profile urls updated with new view path

--- profiles/urls.py
+++ profiles/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
         views.add_to_wishlist,
         name="add_to_wishlist",
     ),
+    path(
+        "remove_from_wishlist/<product_id>/",
+        views.remove_from_wishlist,
+        name="remove_from_wishlist",
+    ),
 ]
