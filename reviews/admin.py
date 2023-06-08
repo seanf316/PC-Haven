a7bddabfdf03446ce656d6cb521b0d1cc8833a61
seanf316 <seanfinn316@gmail.com>
2023-06-08 01:13:52 +0000

    Review model registered in admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+    """
+    Admin registration and configuration for the Review model
+    Staff can see all reviews and filter them as
+    desired
+    """
+
+    list_display = (
+        "user",
+        "product",
+        "rating",
+    )
+
+    search_fields = (
+        "user",
+        "product",
+    )
+
+    list_filter = (
+        "user",
+        "product",
+        "rating",
+    )
+
+    summer_fields = "review"
