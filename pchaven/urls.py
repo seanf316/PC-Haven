339e0ae5950d634223f5adb035eaad29739e6dfb
seanf316 <seanfinn316@gmail.com>
2023-06-08 01:12:27 +0000

    PC Haven urls updated with review app urls

--- pchaven/urls.py
+++ pchaven/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path("cart/", include("cart.urls")),
     path("checkout/", include("checkout.urls")),
     path("profile/", include("profiles.urls")),
+    path("reviews/", include("reviews.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
