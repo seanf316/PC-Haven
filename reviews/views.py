f909f02d5cae86c599609583c8446483e6f72ff3
seanf316 <seanfinn316@gmail.com>
2023-06-08 20:34:02 +0000

    edit review view created

--- reviews/views.py
+++ reviews/views.py
@@ -38,3 +38,45 @@ def add_review(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required()
+def edit_review(request, product_id, review_id):
+    """
+    Checks the database for the Review.id and then confirms if
+    user matches the review user before allowing user to edit their review
+    """
+    user = request.user
+    product = get_object_or_404(Product, pk=product_id)
+    review = get_object_or_404(Review, pk=review_id)
+
+    if user.is_superuser or user == review.user:
+        if request.method == "POST":
+            form = ReviewForm(request.POST, request.FILES, instance=review)
+            if form.is_valid():
+                review = form.save(commit=False)
+                review.save()
+                messages.success(
+                    request, f"{user.username} your review has been updated"
+                )
+
+                return redirect(reverse("product_detail", args=[product_id]))
+            else:
+                messages.error(
+                    request,
+                    "Review updated failed, please review the form.",
+                )
+        else:
+            form = ReviewForm(instance=review)
+
+    else:
+        messages.error(request, "You are not authorized to edit this review.")
+        return redirect(reverse("product_detail", args=[product_id]))
+
+    context = {
+        "form": form,
+        "product": product,
+        "review": review,
+    }
+
+    return render(request, "reviews/edit_review.html", context)
