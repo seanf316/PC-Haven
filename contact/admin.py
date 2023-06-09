df4b9a24ae91a4e811b529879b909921d11b3c04
seanf316 <seanfinn316@gmail.com>
2023-06-09 22:26:44 +0000

    Contact model registered

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "date_received",
+        "contact_reason",
+        "name",
+        "email",
+        "pending_reply",
+        "marked_as_done",
+    )
+
+    list_filter = ("pending_reply", "marked_as_done", "date_received")
+
+    ordering = ("-date_received",)
+
+
+admin.site.register(Contact, ContactAdmin)
