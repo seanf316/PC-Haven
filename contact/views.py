10666fcee8bac34f4a74c05c75f6f8096a1bb794
seanf316 <seanfinn316@gmail.com>
2023-06-09 22:27:29 +0000

    Contact view created

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    """A view to show the contact page"""
+
+    user = request.user
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            form.save()
+            messages.success(
+                request,
+                f"Thank you {name}, your message has been received!"
+                f"We will be in touch shortly to this email address {email}.",
+            )
+            return redirect(reverse("home"))
+        else:
+            messages.error(
+                request,
+                "Contact failed. Please ensure the form is valid!",
+            )
+
+    else:
+        form = ContactForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "contact/contact.html", context)
