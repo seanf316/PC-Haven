79b4f67a33ebff966670160f63352e951e1e4e4b
seanf316 <seanfinn316@gmail.com>
2023-06-10 20:05:03 +0000

    blog urls updated with delete view path

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("<int:blog_id>/like/", views.like_blog, name="like_blog"),
     path("add/", views.add_blog, name="add_blog"),
     path("edit/<int:blog_id>/", views.edit_blog, name="edit_blog"),
+    path("delete/<int:blog_id>/", views.delete_blog, name="delete_blog"),
 ]
