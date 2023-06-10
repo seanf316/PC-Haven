70a5f711e6a99b96d96e0d25483e7fcae698de3e
seanf316 <seanfinn316@gmail.com>
2023-06-10 19:18:25 +0000

    Blog urls updated with new view

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.blogs, name="blogs"),
     path("<int:blog_id>/", views.blog_detail, name="blog_detail"),
     path("<int:blog_id>/like/", views.like_blog, name="like_blog"),
+    path("add/", views.add_blog, name="add_blog"),
 ]
