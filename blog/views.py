4ee2ce38ee8312803f3ccc6634a24bbd4dd9b05d
seanf316 <seanfinn316@gmail.com>
2023-06-10 10:22:17 +0000

    Blogs view created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Blog
+from .forms import BlogForm
 
-# Create your views here.
+
+def blogs(request):
+    """A view to show the blogs page"""
+
+    blogs = Blog.objects.all()
+
+    template = "blog/blogs.html"
+    context = {
+        "blogs": blogs,
+    }
+
+    return render(request, template, context)
