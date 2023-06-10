a015b1be6e07b6c8b7633c3919be1d0f48e8551b
seanf316 <seanfinn316@gmail.com>
2023-06-10 15:05:46 +0000

    blog_detail & like views created

--- blog/views.py
+++ blog/views.py
@@ -17,3 +17,45 @@ def blogs(request):
     }
 
     return render(request, template, context)
+
+
+def blog_detail(request, blog_id):
+    """A view to show blog details"""
+
+    user = request.user
+    blog = get_object_or_404(Blog, pk=blog_id)
+    liked = False
+
+    if user.is_authenticated:
+        if blog.likes.filter(id=user.id).exists():
+            liked = True
+
+    context = {
+        "blog": blog,
+        "liked": liked,
+    }
+
+    return render(request, "blog/blog_detail.html", context)
+
+
+def like_blog(request, blog_id):
+    """A view to handle liking/unliking on a blog post"""
+
+    user = request.user
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    if user.is_authenticated:
+        if blog.likes.filter(id=user.id).exists():
+            blog.likes.remove(user)
+            messages.success(
+                request, f"{user} you have unliked this Blog post."
+            )
+        else:
+            blog.likes.add(user)
+            messages.success(request, f"{user} you have liked this Blog post.")
+    else:
+        messages.info(
+            request, "Only registered users can like this Blog post."
+        )
+
+    return redirect(reverse("blog_detail", args=[blog.id]))
