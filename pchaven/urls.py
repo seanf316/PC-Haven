435fc2f0c604dbc5f4c3bcd6cfaac42cb1f45e35
seanf316 <seanfinn316@gmail.com>
2023-06-10 10:24:28 +0000

    Main urls updated with Blog urls

--- pchaven/urls.py
+++ pchaven/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("profile/", include("profiles.urls")),
     path("reviews/", include("reviews.urls")),
     path("contact/", include("contact.urls")),
+    path("blog/", include("blog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
