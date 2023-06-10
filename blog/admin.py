0d44682008c879b8422284962d17aceaab9a5084
seanf316 <seanfinn316@gmail.com>
2023-06-10 10:23:13 +0000

    blog model registered

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Blog
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+class BlogAdmin(SummernoteModelAdmin):
+    list_filter = ("created_on",)
+    list_display = (
+        "title",
+        "author",
+        "created_on",
+    )
+    search_fields = ["title", "content"]
+    summernote_fields = "content"
+
+
+admin.site.register(Blog, BlogAdmin)
