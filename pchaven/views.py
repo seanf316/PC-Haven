f2f9d294e1aa045a238daef94c2aa3da234e7462
seanf316 <seanfinn316@gmail.com>
2023-06-10 22:35:48 +0000

    Created views for errors in main directory

---
+++ pchaven/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+def error_403(request, exception):
+    """ "
+    Handles HTTP 403 errors
+    """
+
+    return render(request, "errors/403.html")
+
+
+def error_404(request, exception):
+    """ "
+    Handles HTTP 404 errors
+    """
+
+    return render(request, "errors/404.html")
+
+
+def error_500(
+    request,
+):
+    """ "
+    Handles HTTP 500 errors
+    """
+    return render(request, "errors/500.html")
