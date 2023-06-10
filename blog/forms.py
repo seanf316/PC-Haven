5a73f44b4f89771afff91528c98e300ca0beaf8d
seanf316 <seanfinn316@gmail.com>
2023-06-10 10:22:34 +0000

    Blog form  created

---
+++ blog/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django_summernote.widgets import SummernoteWidget
+from .models import Blog
+
+
+class BlogForm(forms.ModelForm):
+    """
+    Form to Create/Edit Blog
+    """
+
+    class Meta:
+        """
+        Define model, form fields and widgets
+        """
+
+        model = Blog
+        fields = "__all__"
+        exclude = ["sku", "in_stock", "image_url"]
+        labels = {
+            "review": "Review",
+            "rating": "Rating (1-5)",
+        }
+
+        widgets = {"review": SummernoteWidget()}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        """
+        Initializes the form attributes
+        """
+        placeholders = {
+            "title": "Enter Blog Title",
+        }
+        self.fields["title"].widget.attrs["autofocus"] = True
