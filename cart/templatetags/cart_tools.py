6db43d1484349775431cba044f3fb618fd3f7c08
seanf316 <seanfinn316@gmail.com>
2023-06-11 18:47:18 +0000

    Added doc string to cart tools

--- cart/templatetags/cart_tools.py
+++ cart/templatetags/cart_tools.py
@@ -5,4 +5,5 @@ register = template.Library()
 
 @register.filter(name="calc_subtotal")
 def calc_subtotal(price, quantity):
+    """Function to calculate the subtotal"""
     return price * quantity
