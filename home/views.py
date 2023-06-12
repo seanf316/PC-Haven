0619b9b1afcf8acc63cee9659f1286455da81d4b
seanf316 <seanfinn316@gmail.com>
2023-06-12 21:36:55 +0000

    Home view updated with featured/latest products

--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
+
 
 def home(request):
-    """ A view to return the home page """
-    return render(request, 'home/index.html')
+    """A view to return the home page"""
+    featured_products = Product.objects.filter(featured_product=True)
+    latest_products = Product.objects.order_by("-created_on")[:10]
+    print(featured_products)
+    context = {
+        "featured_products": featured_products,
+        "latest_products": latest_products,
+    }
+    return render(request, "home/index.html", context)
