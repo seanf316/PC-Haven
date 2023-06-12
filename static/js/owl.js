8fe6b042cb4531e17eae2f4705521a6a31aa15c1
seanf316 <seanfinn316@gmail.com>
2023-06-12 21:37:09 +0000

    owl.js created

---
+++ static/js/owl.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Owl Carousel 
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 15,
+        dots: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: false,
+                loop: true,
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+})
