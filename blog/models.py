66b228d5f5a9a491fefe2ea4b12244dacbde1b8d
seanf316 <seanfinn316@gmail.com>
2023-06-15 03:49:42 +0000

    Comment model added

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_resized import ResizedImageField
+from django.core.validators import (
+    MinLengthValidator,
+    MaxLengthValidator,
+)
 
 
 class Blog(models.Model):
@@ -46,3 +50,37 @@ class Blog(models.Model):
         Returns the Blog likes count
         """
         return self.likes.count()
+
+
+class Comment(models.Model):
+    """
+    Model for creating a Comment
+    """
+
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, related_name="comments"
+    )
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(
+        null=False,
+        blank=False,
+        validators=[MinLengthValidator(1), MaxLengthValidator(300)],
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+    )
+
+    class Meta:
+        """
+        Order set to the created on attribute
+        """
+
+        ordering = ["created_on"]
+
+    def __str__(self):
+        """
+        Returns the User username as a string
+        representation of the object.
+        """
+        return f"Comment by {self.name}"
