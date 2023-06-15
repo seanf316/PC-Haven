0ef7d0ee74efc8fdbeaa743cc0482e9e02b4dace
seanf316 <seanfinn316@gmail.com>
2023-06-15 03:50:35 +0000

    Blog urls updated with add comment

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("add/", views.add_blog, name="add_blog"),
     path("edit/<int:blog_id>/", views.edit_blog, name="edit_blog"),
     path("delete/<int:blog_id>/", views.delete_blog, name="delete_blog"),
+    path("add/comment/<int:blog_id>/", views.add_comment, name="add_comment"),
 ]
