837ff2dc0c6123d0a968b44cdfe2687baef3042c
seanf316 <seanfinn316@gmail.com>
2023-06-16 02:04:39 +0000

    Product urls updated to fix add product error

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.allproducts, name="products"),
-    path("<slug:slug>/", views.product_detail, name="product_detail"),
     path("add/", views.add_product, name="add_product"),
+    path("<slug:slug>/", views.product_detail, name="product_detail"),
     path("edit/<slug:slug>/", views.edit_product, name="edit_product"),
-    path(
-        "delete/<slug:slug>/", views.delete_product, name="delete_product"
-    ),
+    path("delete/<slug:slug>/", views.delete_product, name="delete_product"),
 ]
