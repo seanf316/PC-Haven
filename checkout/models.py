832371761ca588d5830bf216702d8caf4e465f0a
seanf316 <seanfinn316@gmail.com>
2023-06-17 23:04:02 +0000

    Sale references updated on checkout

--- checkout/models.py
+++ checkout/models.py
@@ -114,7 +114,10 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        if self.product.has_sale:
+            self.lineitem_total = self.product.sale_price * self.quantity
+        else:
+            self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
