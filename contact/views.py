707121e7570c82aa4eb30d85e0c558c3976ff028
seanf316 <seanfinn316@gmail.com>
2023-06-19 20:54:28 +0000

    Added contact email to contact app

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from .forms import ContactForm
 
 
@@ -8,16 +11,32 @@ def contact(request):
 
     user = request.user
 
+    subject = "Thank you for contacting PC HAVEN"
+    from_email = "pchaven.com"
+
+    html_message = render_to_string(
+        "contact/emails/contact_email.html",
+    )
+    plain_message = strip_tags(html_message)
+
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data["name"]
             email = form.cleaned_data["email"]
+            to_email = [email]
             form.save()
             messages.success(
                 request,
                 f"Thank you {name}, your message has been received!"
                 f"We will be in touch shortly to this email address {email}.",
+            )
+            send_mail(
+                subject,
+                plain_message,
+                from_email,
+                to_email,
+                html_message=html_message,
             )
             return redirect(reverse("home"))
         else:
