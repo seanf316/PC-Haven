da7ffa428d196b9239f8eb3d0868b2fde5b9958e
seanf316 <seanfinn316@gmail.com>
2023-06-24 22:22:23 +0000

    Robots.txt issue fix(Documented in README)

--- pchaven/urls.py
+++ pchaven/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -30,6 +31,9 @@ urlpatterns = [
     path("reviews/", include("reviews.urls")),
     path("contact/", include("contact.urls")),
     path("blog/", include("blog.urls")),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler403 = "pchaven.views.error_403"
