a3914bf1b7780d2f4d78cacd9402422fbeb1a319
seanf316 <seanfinn316@gmail.com>
2023-07-01 02:02:46 +0000

    Home unitests created

---
+++ home/test_views.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestHomeViews(TestCase):
+    """
+    Testing Home View
+    """
+
+    def test_home_page_renders(self):
+        """
+        Test that home view renders correct page
+        """
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/index.html")
+
+    def test_privacy_page_renders(self):
+        """
+        Test that privacy view renders correct page
+        """
+        response = self.client.get(reverse("privacy"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/privacy_policy.html")
+
+    def test_terms_page_renders(self):
+        """
+        Test that terms and conditions view renders correct page
+        """
+        response = self.client.get(reverse("terms"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/terms.html")
+    
+    def test_faq_page_renders(self):
+        """
+        Test that FAQ view renders correct page
+        """
+        response = self.client.get(reverse("faq"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/faq.html")
