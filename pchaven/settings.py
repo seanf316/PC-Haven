25b1b88e9a690b04fe56145094ae76f0327379d0
seanf316 <seanfinn316@gmail.com>
2024-09-15 22:13:57 +0000

    Swapping AWS for Cloudinary

--- pchaven/settings.py
+++ pchaven/settings.py
@@ -34,13 +34,8 @@ DEBUG = "DEVELOPMENT" in os.environ
 ALLOWED_HOSTS = [
     "pc-haven.herokuapp.com",
     "localhost",
-    "8000-seanf316-pchaven-lppnru9p4ne.ws-eu101.gitpod.io",
+    "8000-seanf316-pchaven-lppnru9p4ne.ws-eu116.gitpod.io",
 ]
-# Add Render.com URL to allowed hosts
-RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -54,9 +49,11 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "cloudinary_storage",
     # Other
     "crispy_forms",
     "storages",
+    "cloudinary",
     "django_summernote",
     # Apps
     "home",
@@ -174,34 +171,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/PC-Haven/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/PC-Haven/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-if "USE_AWS" in os.environ:
-    # Cache control
-    AWS_S3_OBJECT_PARAMETERS = {
-        "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
-        "CacheControl": "max-age=94608000",
-    }
-    # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = "pc-haven"
-    AWS_S3_REGION_NAME = "eu-west-1"
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-
-    # Static and media files
-    STATICFILES_STORAGE = "custom_storages.StaticStorage"
-    STATICFILES_LOCATION = "static"
-    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
-    MEDIAFILES_LOCATION = "media"
-
-    # Override static and media URLs in production
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
 
 # Default primary key field type
